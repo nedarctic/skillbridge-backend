51818dd760336d2b9a40085507767584ce3d5d82
justkimtai <justkimtai@gmail.com>
2025-11-12 09:49:05 +0300

    cleaned up old auth routes to pave way for jwts

--- skillbridge_backend_project/urls.py
+++ skillbridge_backend_project/urls.py
@@ -1,32 +1,23 @@
-"""
-URL configuration for skillbridge_backend_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('profiles.urls')),
+
+    # JWT endpoints
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Optional: DRF browsable API login/logout
     path('api-auth/', include('rest_framework.urls')),
+
+    # dj-rest-auth routes (they can also be configured to use JWT)
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
